a5eab780ed7ca9e1e832af93babc33e5921f11bd
jordanC30 <jordancruz3003@gmail.com>
2026-02-26 12:03:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -29,9 +29,9 @@ function createMovieCard(movie, index) {
           <div class="movie-card-title">${movie.title}</div>
           <div class="movie-card-meta">
             <div class="rating-pill">
-              <svg viewBox="0 0 24 24">
-                <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/>
-              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
+  <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+</svg>
               <span>${movie.imdbRating}</span>
             </div>
             <span>${movie.year}</span>
@@ -44,22 +44,26 @@ function createMovieCard(movie, index) {
         </div>
         <div class="movie-card-bottom">
           <div class="movie-card-buttons">
-            <button class="card-btn-play" onclick="playMovie(${JSON.stringify(movie).replace(/"/g, '&quot;')})">
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M8 5v14l11-7z"/>
+            <button class="card-btn-play">
+              <svg width="16" height="16" >
+                <path
+                    d="M10.804 8 5 4.633v6.734zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696z" />
               </svg>
               Play
             </button>
-            <button class="card-btn-icon" onclick="addToList(${movie.id})">
-              <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                <line x1="12" y1="5" x2="12" y2="19"></line>
-                <line x1="5" y1="12" x2="19" y2="12"></line>
-              </svg>
+            <button class="card-btn-icon" >
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor"
+                                class="bi bi-plus-lg me-1" viewBox="0 0 13 16">
+                                <path fill-rule="evenodd"
+                                    d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2" />
+                            </svg>
             </button>
-            <button class="card-btn-icon" onclick="showDetails(${JSON.stringify(movie).replace(/"/g, '&quot;')})">
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                <circle cx="12" cy="12" r="10"></circle>
-                <path d="M12 16v-4M12 8h.01"></path>
+            <button class="card-btn-icon">
+              <svg width="35" height="20" fill="currentColor"
+                                class="bi bi-info-circle me-2" viewBox="0 0 9 16">
+                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
+                                <path
+                                    d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0" />
               </svg>
             </button>
           </div>
